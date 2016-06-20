a45986f29e31862bb23b958db038fa2bc6d5b19d
Daniel.Kec <daniel.kec@embedit.cz>
2016-06-20 20:14:18 +0200

    Spring Rest Doc

--- complete/src/test/java/hello/GreetingControllerTests.java
+++ complete/src/test/java/hello/GreetingControllerTests.java
@@ -15,17 +15,23 @@
  */
 package hello;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.restdocs.JUnitRestDocumentation;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -34,38 +40,59 @@ import org.springframework.web.context.WebApplicationContext;
 
 /**
  * @author Greg Turnquist
+ * @author Daniel Kec
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
 @WebAppConfiguration
 public class GreetingControllerTests {
 
-	@Autowired
-	private WebApplicationContext ctx;
+    @Autowired
+    private WebApplicationContext ctx;
 
-	private MockMvc mockMvc;
+    private MockMvc mockMvc;
 
-	@Before
-	public void setUp() {
-		this.mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build();
-	}
+    @Before
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.ctx)
+                .apply(documentationConfiguration(this.restDocumentation)
+                .uris()
+                .withScheme("http")
+                .withHost("embedit.cz")
+                .withPort(8080))
+                .build();
+    }
 
-	@Test
-	public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
+    @Rule
+    public JUnitRestDocumentation restDocumentation =
+            new JUnitRestDocumentation("target/generated-snippets");
 
-		this.mockMvc.perform(get("/greeting"))
-				.andDo(print())
-				.andExpect(status().isOk())
-				.andExpect(jsonPath("$.content").value("Hello, World!"));
-	}
+    @Test
+    public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
 
-	@Test
-	public void paramGreetingShouldReturnTailoredMessage() throws Exception {
+        this.mockMvc.perform(get("/greeting"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").value("Hello, World!"));
+    }
 
-		this.mockMvc.perform(get("/greeting").param("name", "Spring Community"))
-				.andDo(print())
-				.andExpect(status().isOk())
-				.andExpect(jsonPath("$.content").value("Hello, Spring Community!"));
-	}
+    @Test
+    public void paramGreetingShouldReturnTailoredMessage() throws Exception {
+
+        this.mockMvc.perform(
+                get("/greeting")
+                        .param("name", "EmbedIT"))
+                .andDo(document("greeting"
+                        ,
+                        requestParameters(
+                                parameterWithName("name")
+                                        .optional()
+                                        .description("Name to be greeted.")
+                        )
+                ))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").value("Hello, EmbedIT!"));
+    }
+
 
 }
